22651e15897a35578aa8919ec8187a4bf64405b2
Tharun <dugginenitharunkumar25@gmail.com>
2023-01-27 18:25:28 +0530

    first commit

---
+++ nestedloops.py
@@ -0,0 +1,24 @@
+rows = int(input("Enter No. of rows:"))
+# for i in range(rows):
+#     for  j in range(i+1):
+#           print("* ",end="")
+#     print("\n")      
+# for i in range(rows):
+#       for j in range(i+1):
+#             print(j+1,end="")
+#       print("\n")   
+# ascii_value=65   
+# for i in range(rows):
+#       for j in range(i+1):
+#            alpha=chr(ascii_value)
+#            print(alpha ,end=" ")
+#       ascii_value+=1
+#       print("\n")     
+# for i in range(rows,0,-1):
+#       for j in range(i):
+#             print("*",end=" ")
+#       print("\n")      
+for i in range(rows,0,-1):
+      for j in range(i):
+            print(j+1,end=" ")
+      print("\n")      
